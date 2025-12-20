1d8a28cc698a0ebb24e148448c9a790f05bc3032
pedromarq40 <pedromarq400@gmail.com>
2025-12-19 23:15:02 -0300

    Verificação de Vencedor

--- Jogo-Da-Velha/src/App.jsx
+++ Jogo-Da-Velha/src/App.jsx
@@ -19,9 +19,26 @@ function Jogo_Da_Velha(){
   const [quadrados, setQuadrados] = useState(Array(9).fill(null))
   //Cria array vazio com 9 casas e coloca null em todos
 
+  const vencedor = verificarVencedor(quadrados)
+  let status
+  //Cada vez que usa set em um state o componente se refaz, logo, essa função
+  //testa o tabuleiro novo e não o antigo
+
+  if(vencedor){
+    status = `Vencedor é ${vencedor}`
+  } else {
+    status = `Vez do ${ vezDoX ? 'X' : 'O'}`
+  }
+
+
   function marcarJogo(i){
     let novos_quadrados = quadrados.slice()
 
+    if(vencedor){
+      alert('Já há um vencedor!')
+      return
+    }
+    
     if(novos_quadrados[i]){ //Acaba a função caso a peça esteja ocupada
       alert('Esse quadrado já está marcado!')
       return 
@@ -35,10 +52,12 @@ function Jogo_Da_Velha(){
 
     setVezDoX(!vezDoX)
     setQuadrados(novos_quadrados)
+
   }
 
   return (
     <>
+    <h2>{status}</h2>
       <div className='jogo_da_velha'>
         <Quadrado desenho={quadrados[0]} Marcar={() => marcarJogo(0)} />
         <Quadrado desenho={quadrados[1]} Marcar={() => marcarJogo(1)} />
@@ -59,5 +78,30 @@ function Quadrado({desenho, Marcar}){
   return (<button className='quadrado' onClick={Marcar}>
     {desenho}
   </button>)
+}
+
+function verificarVencedor(quadrados){
+
+  const VITORIAS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+  for( let combinacao of VITORIAS ){
+
+    let [a, b, c] = combinacao
+
+    if(quadrados[a] && quadrados[a] === quadrados[b] && quadrados[b] === quadrados[c]){
+      return quadrados[a]
+    }
+  }
+  return null
+
 }
 export default App
