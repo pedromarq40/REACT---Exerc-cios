68db1044799f02b12355db5cdf8c93e8731babd9
pedromarq40 <pedromarq400@gmail.com>
2025-12-20 20:49:23 -0300

    Finalizando o jogo

--- Jogo-Da-Velha/src/App.jsx
+++ Jogo-Da-Velha/src/App.jsx
@@ -5,36 +5,59 @@ import './App.css'
 
 function App() {
   
+  const [historico, setHistorico] = useState([Array(9).fill(null)])
+  const [atualJogada, setAtualJogada] = useState(0)
+  const atuaisQuadrados = historico[atualJogada]
+  const vezDoX = atualJogada % 2 === 0
+
+  function marcarJogo(novos_quadrados){ //Marcar o Jogo
+
+    let novo_historico = historico.slice(0, atualJogada + 1)
+    novo_historico.push(novos_quadrados)
+
+    setAtualJogada(novo_historico.length - 1)
+    setHistorico(novo_historico)
+  }
+
+  function voltarJogadas(i){
+    setAtualJogada(i)
+  }
+
+  const jogadas = historico.map( (jogada, index) => {
+
+    let texto = index === 0 ? 'Inicio' : `Jogada n${index}` 
+
+    return (
+      <li key={index}>
+        <button onClick={ () => voltarJogadas(index)}>{texto}</button>
+      </li>
+    )
+  })
+
   return (
     <>
     <h1>Jogo Da Velha</h1>
-      <Jogo_Da_Velha />
+    <div class='jogo'>
+        <Jogo_Da_Velha quadrados={atuaisQuadrados} marcarJogo={marcarJogo} vezDoX={vezDoX}/>
+
+        <div class='list '>
+          <h3>Lista de Jogadas: </h3>
+          <ol>
+            {jogadas}
+          </ol>
+        </div>
+    </div>
     </>
   )
 }
 
-function Jogo_Da_Velha(){
+function Jogo_Da_Velha({quadrados, marcarJogo, vezDoX}){
+  
+  function pre_marcarJogo(i){
 
-  const [vezDoX, setVezDoX] = useState(true)
-  const [quadrados, setQuadrados] = useState(Array(9).fill(null))
-  //Cria array vazio com 9 casas e coloca null em todos
+    const novos_quadrados = quadrados.slice()
 
-  const vencedor = verificarVencedor(quadrados)
-  let status
-  //Cada vez que usa set em um state o componente se refaz, logo, essa função
-  //testa o tabuleiro novo e não o antigo
-
-  if(vencedor){
-    status = `Vencedor é ${vencedor}`
-  } else {
-    status = `Vez do ${ vezDoX ? 'X' : 'O'}`
-  }
-
-
-  function marcarJogo(i){
-    let novos_quadrados = quadrados.slice()
-
-    if(vencedor){
+    if(verificarVencedor(quadrados)){ //Acaba a função caso tenha vencedor
       alert('Já há um vencedor!')
       return
     }
@@ -44,30 +67,41 @@ function Jogo_Da_Velha(){
       return 
     }
 
-    if (vezDoX){
+    if(vezDoX){
       novos_quadrados[i] = 'X'
-    } else {
+    }else{
       novos_quadrados[i] = 'O'
     }
 
-    setVezDoX(!vezDoX)
-    setQuadrados(novos_quadrados)
+    marcarJogo(novos_quadrados)
 
   }
 
+  //Fora do escopo da função, faz a verificação
+    const vencedor = verificarVencedor(quadrados)
+    let status
+
+    if(vencedor){
+      status = `Vencedor é ${vencedor}`
+    }else{
+      status = `Vez do ${ vezDoX ? 'X' : 'O'}`
+    }
+
   return (
     <>
-    <h2>{status}</h2>
-      <div className='jogo_da_velha'>
-        <Quadrado desenho={quadrados[0]} Marcar={() => marcarJogo(0)} />
-        <Quadrado desenho={quadrados[1]} Marcar={() => marcarJogo(1)} />
-        <Quadrado desenho={quadrados[2]} Marcar={() => marcarJogo(2)} />
-        <Quadrado desenho={quadrados[3]} Marcar={() => marcarJogo(3)} />
-        <Quadrado desenho={quadrados[4]} Marcar={() => marcarJogo(4)} />
-        <Quadrado desenho={quadrados[5]} Marcar={() => marcarJogo(5)} />
-        <Quadrado desenho={quadrados[6]} Marcar={() => marcarJogo(6)} />
-        <Quadrado desenho={quadrados[7]} Marcar={() => marcarJogo(7)} />
-        <Quadrado desenho={quadrados[8]} Marcar={() => marcarJogo(8)} />
+      <div>
+        <h2>{status}</h2>
+          <div className='jogo_da_velha'>
+            <Quadrado desenho={quadrados[0]} Marcar={() => pre_marcarJogo(0)} />
+            <Quadrado desenho={quadrados[1]} Marcar={() => pre_marcarJogo(1)} />
+            <Quadrado desenho={quadrados[2]} Marcar={() => pre_marcarJogo(2)} />
+            <Quadrado desenho={quadrados[3]} Marcar={() => pre_marcarJogo(3)} />
+            <Quadrado desenho={quadrados[4]} Marcar={() => pre_marcarJogo(4)} />
+            <Quadrado desenho={quadrados[5]} Marcar={() => pre_marcarJogo(5)} />
+            <Quadrado desenho={quadrados[6]} Marcar={() => pre_marcarJogo(6)} />
+            <Quadrado desenho={quadrados[7]} Marcar={() => pre_marcarJogo(7)} />
+            <Quadrado desenho={quadrados[8]} Marcar={() => pre_marcarJogo(8)} />
+          </div>
       </div>
     </>
   )
